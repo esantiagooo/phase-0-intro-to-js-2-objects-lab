cf40c25dbde2697853922d7ee2ababba208abfa7
Eric <eric.yanez50@qmail.cuny.edu>
2022-04-18 22:53:17 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+const employee = {}
+employee.name = 'eric'
+employee['streetAddress'] = 'New York'
+console.log(employee)
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const newobj = {...employee}
+    newobj[key] = value
+    return newobj
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key, value){
+    employee[key]= value
+    return employee
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    const newobj = {...employee}
+    delete newobj[key]
+    return newobj
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key]
+    return employee
+}
